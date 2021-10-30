950a7bfe5bcf8caaf7d7db7739040088458901ce
James Vaclavek <jamievacl@gmail.com>
2021-10-30 16:47:23 -0400

    Mobile Responsive Edits #3

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Layout from '../components/Layout'
 import Meta from '../components/Meta'
 import Navigation from '../components/Navigation'
 import { Container, Row, Col, Button, Image } from 'react-bootstrap';
